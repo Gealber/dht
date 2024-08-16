aafbc5d4c619a65c613ef3a8374299f9264cf580
Gealber <gealbermorales@gmail.com>
2024-08-16 19:09:27 +0200

    update example and TODO, example is broken needs final steps

--- adnl/example/example.go
+++ adnl/example/example.go
@@ -3,7 +3,10 @@ package main
 import (
 	"crypto/ed25519"
 	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
 	"encoding/binary"
+	"encoding/hex"
 	"fmt"
 	"log"
 	"net"
@@ -20,16 +23,18 @@ func main() {
 		log.Fatal(err)
 	}
 
+	tlHandler := tl.New()
+
 	node := cfg.Dht.StaticNodes.Nodes[0]
 
 	ipDec := node.AddrList.Addrs[0].IP
 	port := node.AddrList.Addrs[0].Port
-	// key := node.ID.Key
+	key := node.ID.Key
 
-	// keyB, err := base64.RawStdEncoding.DecodeString(key)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	dhtNodeKey, err := base64.RawStdEncoding.DecodeString(key)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	// convert decimal ip to normal ip formatting
 	ip := make(net.IP, 4)
@@ -42,93 +47,92 @@ func main() {
 	}
 	defer conn.Close()
 
-	// first let's send create-channel
 	// adnl.message.createChannel key:int256 date:int = adnl.Message;
-	pub, _, err := ed25519.GenerateKey(rand.Reader)
+	channelKey, _, err := ed25519.GenerateKey(rand.Reader)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	msgCreateChannel := adnl.CreateChannel{
-		Key:  pub,
-		Date: time.Now().Unix(),
+	date := time.Now().Unix()
+	createChn := adnl.CreateChannel{
+		Key:  channelKey,
+		Date: date,
 	}
 
-	tlHandler := tl.New()
-
-	createChannelData, err := tlHandler.Serialize(msgCreateChannel, true)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// // sending create channel request
-	// n, err := conn.Write(createChannelData)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// log.Printf("written %d byte\n", n)
-	log.Printf("%x", createChannelData)
-
-	// second message will be the actual query
-	// serialize GetSignedAddressList
-	var addrList adnl.GetSignedAddressList
-
-	query, err := tlHandler.Serialize(addrList, true)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// adnl.message.query query_id:int256 query:bytes = adnl.Message
 	queryID := make([]byte, 32)
 	rand.Read(queryID)
-	adnlQuery := adnl.Query{
+
+	query, err := tlHandler.Serialize(adnl.GetSignedAddressList{}, true)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("GET SIGNED ADDRESS LIST QUERY: ", hex.EncodeToString(query))
+
+	msgQuery := adnl.Query{
 		QueryID: queryID,
 		Query:   query,
 	}
 
-	msgQueryData, err := tlHandler.Serialize(adnlQuery, true)
+	ourPub, ourPk, err := ed25519.GenerateKey(rand.Reader)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Printf("%x", msgQueryData)
+	buff := make([]byte, 30)
+	rand.Read(buff)
+	rand1, rand2 := buff[:15], buff[15:]
 
-	// building packet
-	r1 := make([]byte, 15)
-	rand.Read(r1)
-	r2 := make([]byte, 15)
-	rand.Read(r2)
-	log.Printf("RAND1: %x\n", r1)
-	log.Printf("RAND2: %x\n", r2)
-
-	var seqno int64 = 1
-	var confirmSeqno int64 = 0
-	tm := int32(time.Now().Unix())
-
-	packet := adnl.PacketContent{
-		Rand1: r1,
-		Flags: 0xd9050000,
+	pkt := adnl.PacketContent{
+		Rand1: rand1,
+		Flags: 0x05d9,
 		From: adnl.PublicKeyED25519{
-			Key: pub,
+			Key: ourPub,
 		},
-		Messages: []any{msgCreateChannel, msgQueryData},
-		Address: &adnl.List{
-			Addresses:  []*adnl.UDP{},
-			Version:    tm,
-			ReinitDate: tm,
+		Messages: []any{
+			createChn,
+			msgQuery,
 		},
-		Seqno:               &seqno,
-		ConfirmSeqno:        &confirmSeqno,
-		RecvAddrListVersion: &tm,
-		ReinitDate:          &tm,
-		Rand2:               r2,
+		AddressList: adnl.List{
+			Addresses:  []adnl.UDP{},
+			Version:    date,
+			ReinitDate: date,
+			Priority:   0,
+			ExpireAt:   0,
+		},
+		Seqno:               1,
+		ConfirmSeqno:        0,
+		RecvAddrListVersion: date,
+		ReinitDate:          date,
+		DstReinitDate:       0,
+		Rand2:               rand2,
 	}
 
-	pktData, err := tlHandler.Serialize(packet, true)
+	models := []tl.ModelRegister{
+		{T: adnl.CreateChannel{}, Def: adnl.TLCreateChannel},
+		{T: adnl.GetSignedAddressList{}, Def: adnl.TLSignedAddressList},
+		{T: adnl.PublicKeyED25519{}, Def: adnl.TLPublicKeyEd25519},
+		{T: adnl.Query{}, Def: adnl.TLMessageQuery},
+		{T: adnl.UDP{}, Def: adnl.TLAddressUDP},
+		{T: adnl.List{}, Def: adnl.TLAddressList},
+	}
+	tlHandler.Register(models)
+
+	data, err := tlHandler.Serialize(pkt, true)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Printf("%x", pktData)
+	pkt.Signature = ed25519.Sign(ourPk, data)
+	// serialize again, this time with signature included
+	// so we need to enable the 11th bit on flag which
+	pkt.Flags = 0x0dd9
+
+	data, err = tlHandler.Serialize(pkt, true)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	h := sha256.Sum256(data)
+
 }
